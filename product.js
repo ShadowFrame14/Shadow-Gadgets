517922182506d9851d1d81ea808f5837d6cd0d7f
ShadowFrame14 <alzatedarwin13@gmail.com>
2026-02-22 16:40:12 -0500

    Create product.js

---
+++ product.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    name: "Smartwatch Pro",
+    price: "89.900 COP",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+  },
+  {
+    name: "Auriculares Bluetooth",
+    price: "59.900 COP",
+    image: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad"
+  },
+  {
+    name: "Zapatillas Urban",
+    price: "129.900 COP",
+    image: "https://images.unsplash.com/photo-1606813907291-d86efa9b94db"
+  }
+];
